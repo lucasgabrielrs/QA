e34689c201651196e755de70ff3a13f2631c6cdf
Lucas Gabriel <lucasgabrielrs123@gmail.com>
2024-07-21 13:52:24 -0300

    addDeleteRomaneio

--- APItests/smokeTests/K6tests/romaneioApi.js
+++ APItests/smokeTests/K6tests/romaneioApi.js
@@ -3,7 +3,7 @@ import { check, sleep } from "k6";
 import { baseURL, params } from "../config.js";
 
 export const options = {
-  vus: 2,
+  vus: 1,
   iterations: 2,
 };
 
@@ -24,12 +24,37 @@ export default function () {
     }
 
     let mensagem = body.mensagem;
+    var numero = body.romaneio.numero;
     console.log("Mensagem:", mensagem);
+    console.log("Numero: ", numero);
 
     check(resp, {
       "Status should be 200": (r) => r.status === 200,
     });
   } else {
     console.error("Request failed. Status code:", resp.status);
+  }
+
+  sleep(2);
+
+  // Deletando
+  const del = http.del(
+    `${baseURL}/romaneios/excluir?grupo_emp=AZAPFY&timezone=America%2FSao_Paulo&numero=${numero}`
+  );
+
+  if (del.status === 200) {
+    let delBody;
+    try {
+      delBody = JSON.parse(del.body);
+    } catch (e) {
+      console.error("Erro ao parsear a resposta JSON:", e);
+      return;
+    }
+
+    check(del, {
+      "Status delete should be 200": (r) => r.status === 200,
+    });
+  } else {
+    console.error("Request failed. Status code:", del.status);
   }
 }
